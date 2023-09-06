ed9b4826f54716251f65aa0042f8f51b675c12e6
ZionSpardas <135459718+ZionSpardas@users.noreply.github.com>
2023-09-06 19:38:14 +0000

    I cant move Things around

---
+++ src/js/component/List.jsx
@@ -0,0 +1,7 @@
+import React, { useState }  from "react";
+import propTypes from "prop-types";
+
+const List = () =>{
+
+
+export default List;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [item, setItem] = useState([]);
+	const [input, setInput] = useState("");
+
+	const saveInput = (e) => {
+		setInput(e.target.value);
+	};
+
+	const addNewItem = (event) => {
+		if (event.key === 'Enter') {
+			const copyCart = [...item];
+			copyCart.push(input);
+			setItem(copyCart);
+			setInput("");
+		}
+	};
+
+	const deleteTask = (id) =>{
+		const newItem = item.filter((_, i) => i !== id);
+    	setItem(newItem);
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid ">
+			<div className="col-6 mx-auto my-5">
+				<div className="row border border-2 boder-secondary">
+					
+						<div className="input-group-lg pt-3 d-flex ">
+							<input className="form-control border-0 mx-2 fs-3" type="text" placeholder="Inset New Text" onChange={saveInput} onKeyDown={addNewItem} value={input}></input>
+							
+						</div>
+					
+				</div>
+				
+				{item.map((subItems, id) => {
+					return (
+						<div className="row border border-2 border-top-0 py-2">
+							
+								
+								<li  className="d-flex w-100 float-end" key={id}> 
+								<span className="fs-3 mx-4 align-items-start"> {subItems}</span>
+								
+								<button type="button" className="btn-close border border-0 mt-2 btn-secondary float-end" onClick={() => deleteTask(id)}></button>
+								</li>
+							
+						</div>
+					)
+				})}
+				
+				{item.length > 0 ? (
+					<div className="row border border-2 border-top-0 py-2">
+						<span> You still have {item.length} tasks</span>
+					</div>
+				) : (
+					<div className="row border border-2 border-top-0 py-2">
+						<span> You dont have any task to do Perfect !! </span>
+					</div>
+				)}
+
+			</div>
+
 		</div>
 	);
 };
